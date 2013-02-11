e0508f48d80750e84e6548f32ce770ca64c7759e
nishacodes <nishabatra27@gmail.com>
2013-02-10 21:28:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pixelove::Application.config.secret_token = 'ca884be0a366d5db5705688c91d1fe876e720a278f37ac48b6d157ba40e90f05c70b18f31ceffcb43ff8c37c48ab4485e1bff88ff0c6041338a3ab77078400fc'
